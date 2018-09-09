355f72f75d4e9287822301af0b942401601c313b
wad3inthewater <wfulle20@gmail.com>
2018-09-09 14:50:29 -0400

    delete the contents of the build folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,42 @@
-// import React, { PureComponent } from 'react';
-import React from 'react';
+import React, { PureComponent } from 'react';
+import classNames from 'classnames';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 
 // export default class Header extends PureComponent {
 
 // }
 
-export default function Header() {
-  return (
-    <header className="header">
-      <div className="left-panel">
-        80*
+export default class Header extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mounted: false
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      mounted: true
+    });
+  }
+  render() {
+    const { mounted } = this.state;
+    const rightPanelclasses = classNames('right-panel', {
+      'runanimation': mounted
+    })
+    return (
+      <header className="header">
+        <div className="left-panel">
+          80*
       </div>
-      <div className="center-panel"><h1>something here</h1>
-        <p>please enter the passcode here things here</p>
-      </div>
-      <div className="right-panel">
-        <p>another thing</p>
-        <div className="right-panel-outline"></div>
-      </div>
-    </header >
-  )
+        <div className="center-panel"><h1>something here</h1>
+          <p>please enter the passcode here things here</p>
+        </div>
+        <div className={rightPanelclasses}>
+          <p>another thing</p>
+          <div className="right-panel-outline"></div>
+        </div>
+      </header >
+    )
+  }
 }
