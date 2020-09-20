cdef911e4086098248063b1abf1892fabadacf1b
wad3inthewater <wfulle20@gmail.com>
2020-09-20 18:59:36 -0400

    testing CICD

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ export default class Header extends PureComponent {
           <div className="boarder-highlight delay-animation-extended-1"></div>
           <div className="content-wrap">
             <Link className="no-link-style" to="/">
-              <h1 className="main-header">Wade Fuller III*</h1>
+              <h1 className="main-header">Wade Fuller III</h1>
             </Link>
           </div>
         </div>
--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export default function SkillsSection() {
   return (
@@ -17,20 +17,17 @@ export default function SkillsSection() {
           <div className="spider-web-wrap web-spin">
             <div class="web">
               <div class="outter outter-3">
-                <div class="inner">
-                </div>
+                <div class="inner"></div>
               </div>
               <div class="outter outter-2">
-                <div class="inner">
-                </div>
+                <div class="inner"></div>
               </div>
               <div class="outter">
                 <div className="web-line"></div>
                 <div className="web-line-2"></div>
                 <div className="web-line-3"></div>
                 <div className="web-line-4"></div>
-                <div class="inner">
-                </div>
+                <div class="inner"></div>
               </div>
             </div>
           </div>
@@ -39,11 +36,11 @@ export default function SkillsSection() {
           <h2>Pixel Pushing</h2>
           <ul class="skills-list no-list-style">
             <li>Foundation in visual arts</li>
-            <li>Making it happen with any program that has a pen tool</li>
+            <li>Making it happen with any program that has a pen tool -></li>
             <li>Sketch it, scan it, add the magic</li>
           </ul>
         </div>
       </section>
     </React.Fragment>
-  )
+  );
 }
